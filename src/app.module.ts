1dedca75a51835e04a0ea843a12934f884ae6b83
VD4EK <svr1884@gmail.com>
2025-03-29 18:36:38 +0500

    Тест

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import {Module} from "@nestjs/common"
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import { UsersModule } from './users/users.module';
+import * as process from "node:process";
 
+if(!process.env.NODE_ENV){
+    process.env.NODE_ENV = 'production'
+}
 
 @Module( {
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [],
+  providers: [],
+  imports: [
+      ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: `.env.${process.env.NODE_ENV}.local`,
+      }),
+      TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: process.env.POSTGRES_HOST,
+          port: Number(process.env.POSTGRES_PORT),
+          username: process.env.POSTGRES_USER,
+          password: process.env.POSTGRES_PASSWORD,
+          database: process.env.POSTGRES_DB,
+          entities: [],
+          autoLoadEntities: true,
+      }),
+      UsersModule
+  ]
 })
 export class AppModule {}
